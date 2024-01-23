c98cf53e78c821ac2ad1d34609f36f4d95af7d4d
Oguz Tec <69775148+oguztecirlioglu@users.noreply.github.com>
2024-01-23 18:58:49 +0000

    Added refurbed service

---
+++ refurbed-service/src/main/java/com/github/oguztecirlioglu/refurbedservice/RefurbedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.oguztecirlioglu.refurbedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefurbedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefurbedServiceApplication.class, args);
+	}
+
+}
